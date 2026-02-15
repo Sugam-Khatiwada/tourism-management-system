2f2f45298e194e4f9e072f4c002381a4cad614d7
Sugam-Khatiwada <201203380+Sugam-Khatiwada@users.noreply.github.com>
2026-02-15 11:14:46 +0545

    fix(UserCRUD): Fix user validation

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -4,14 +4,14 @@ export const createUserSchema = z.object({
     name: z.string().min(1, "Name is required"),
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, "Password must be at least 6 characters long"),
-    role: z.enum(["admin", "user"], "Role must be either 'admin' or 'user'").optional(),
+    role: z.enum(["admin", "tourist","guide","hotel_owner","bus_owner"], "Role must be either 'admin', 'tourist', 'guide', 'hotel_owner', or 'bus_owner'").optional(),
 });
 
 export const updateUserSchema = z.object({
     name: z.string().min(1, "Name is required").optional(),
     email: z.string().email("Invalid email address").optional(),
     password: z.string().min(6, "Password must be at least 6 characters long").optional(),
-    role: z.enum(["admin", "user"], "Role must be either 'admin' or 'user'").optional(),
+    role: z.enum(["admin", "tourist","guide","hotel_owner","bus_owner"], "Role must be either 'admin', 'tourist', 'guide', 'hotel_owner', or 'bus_owner'").optional(),
 });
 
 export type CreateUserDto = z.infer<typeof createUserSchema>;
