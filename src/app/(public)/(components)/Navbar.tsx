5cf497902b3830ca5a8d3f99e1c78de52a0ca528
Sugam-Khatiwada <201203380+Sugam-Khatiwada@users.noreply.github.com>
2026-02-20 07:38:49 +0545

    feat: add Navbar component

---
+++ src/app/(public)/(components)/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center p-6 shadow">
+      <h1 className="text-xl font-bold">TourismMS</h1>
+
+      <div className="flex gap-6">
+        <Link href="/search">Explore</Link>
+        <Link href="/auth/login">Login</Link>
+        <Link href="/auth/register" className="bg-black text-white px-4 py-2 rounded">
+          Sign Up
+        </Link>
+      </div>
+    </nav>
+  );
+}
